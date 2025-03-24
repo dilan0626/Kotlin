eda9c4a394ed274fece315e8af0f521831b92589
dilan0626 <dilan.jesus.carranza@gmail.com>
2025-03-24 11:10:45 -0600

    segundo commit

---
+++ src/1_Tipos_de_variables/Ejemols.kt
@@ -0,0 +1,35 @@
+package `1_Tipos_de_variables`
+
+fun main() {
+    //Todas las variables val son inmutables
+// Variable inmutable de tipo entero
+    val edad: Int
+    edad = 48
+    //Variable inmutable de tipo decimal
+    val sueldo: Float
+    sueldo = 1200.55f // siempre lleva una "f" al final del valor
+    //Variable inmutable de tipo decimal
+    val total: Double
+    total = 70000.24
+    // Variable inmutable de tipo alfanumerico
+    val titulo: String
+    titulo = "Sistema de ventas"
+    // Variable mutable, las variables mutables empiezan con "var"
+    var mes: Int
+    mes = 1
+    //algunas lineas mas de codigo
+    mes = 2
+    // Variable de tipo Char, almacna un unico caracter
+    var continuar: Char = 's'
+    // Variable de tipo Booleano, true, false
+    var fin: Boolean = false
+
+// lo crea seguido
+    print("Dilan")
+    print("jesus")
+
+    //crea un salto de linea
+    println("Carranza")
+    println("Enriquez")
+
+}
---
+++ src/1_Tipos_de_variables/Proyecto2.kt
@@ -0,0 +1,20 @@
+package `1_Tipos_de_variables`
+
+fun main() {
+
+    val dig1: Int
+    dig1 = 5
+
+    val dig2: Int
+    dig2 = 5
+
+    var suma: Int
+    suma = dig1 + dig2
+
+    var producto: Int
+    producto = suma
+
+    print("La suma es = 10 ")
+
+
+}
